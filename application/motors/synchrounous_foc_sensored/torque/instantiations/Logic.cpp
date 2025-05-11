56a0a5ed0ab0b043954bbe5951f2c4cb61ca7a5d
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-05-11 18:41:16 +0000

    add debug led

--- application/motors/synchrounous_foc_sensored/torque/instantiations/Logic.cpp
+++ application/motors/synchrounous_foc_sensored/torque/instantiations/Logic.cpp
@@ -5,7 +5,8 @@ namespace application
     Logic::Logic(application::HardwareFactory& hardware)
         : motorFocImpl{ hardware.MotorFieldOrientedController(), hardware.MotorPosition(), focImpl }
         , focInteractor{ motorFocImpl }
-        , terminalWithStorage(hardware.Terminal(), hardware.Tracer())
-        , terminal(terminalWithStorage, focInteractor)
+        , terminalWithStorage{ hardware.Terminal(), hardware.Tracer() }
+        , terminal{ terminalWithStorage, focInteractor }
+        , debugLed{ hardware.Leds().front(), std::chrono::milliseconds(50), std::chrono::milliseconds(1950) }
     {}
 }
