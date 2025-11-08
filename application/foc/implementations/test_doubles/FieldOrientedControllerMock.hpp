7126191754a8e1c8855722d31c0f98b97ecb3e12
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-11-08 20:32:29 +0000

    add tests for speed controller

--- application/foc/implementations/test_doubles/FieldOrientedControllerMock.hpp
+++ application/foc/implementations/test_doubles/FieldOrientedControllerMock.hpp
@@ -16,4 +16,15 @@ namespace foc
         MOCK_METHOD(void, SetTunings, (Volts Vdc, const IdAndIqTunings& tunings), (override));
         MOCK_METHOD(PhasePwmDutyCycles, Calculate, (const PhaseCurrents& currentPhases, Radians& position), (override));
     };
+
+    class FieldOrientedControllerSpeedControlMock
+        : public FieldOrientedControllerSpeedControl
+    {
+    public:
+        MOCK_METHOD(void, SetPolePairs, (std::size_t polePairs), (override));
+        MOCK_METHOD(void, Reset, (), (override));
+        MOCK_METHOD(void, SetPoint, (RadiansPerSecond), (override));
+        MOCK_METHOD(void, SetTunings, (Volts Vdc, const SpeedTunings& speedTuning, const IdAndIqTunings& torqueTunings), (override));
+        MOCK_METHOD(PhasePwmDutyCycles, Calculate, (const PhaseCurrents& currentPhases, Radians& position), (override));
+    };
 }
