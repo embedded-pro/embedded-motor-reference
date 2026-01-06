2e404bdc427a700b776a6d871c77626e59ad8795
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-01-06 18:16:54 +0000

    remove banner from components/terminal

--- source/application/hardware_test/components/Terminal.cpp
+++ source/application/hardware_test/components/Terminal.cpp
@@ -128,28 +128,6 @@ namespace application
         encoderCreator.Emplace();
         pwmCreator.Emplace(std::chrono::nanoseconds{ 500 }, hal::Hertz{ 10000 });
         StartAdc(HardwareFactory::SampleAndHold::shortest);
-
-        PrintHeader();
-    }
-
-    void TerminalInteractor::PrintHeader()
-    {
-        tracer.Trace() << "================================================";
-        tracer.Trace() << "e-foc:hardware_test";
-        tracer.Trace() << "Version: 0.0.1";
-        tracer.Trace() << "Build: " << __DATE__ << " " << __TIME__;
-#ifdef __VERSION__
-        tracer.Trace() << "Compiler: " << __VERSION__;
-#elif defined(_MSC_VER)
-        tracer.Trace() << "Compiler: MSVC " << _MSC_VER;
-#else
-        tracer.Trace() << "Compiler: Unknown";
-#endif
-        tracer.Trace() << "Target: " << E_FOC_TARGET_BOARD;
-        tracer.Trace() << "System Clock: " << systemClock.Value() << " Hz";
-        tracer.Trace() << "Power Supply Voltage: " << Vdc.Value() << " V";
-        tracer.Trace() << "================================================";
-        tracer.Trace() << "Ready to accept commands. Type 'help' for available commands.";
     }
 
     TerminalInteractor::StatusWithMessage TerminalInteractor::ConfigurePwm(const infra::BoundedConstString& param)
